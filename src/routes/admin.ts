8e7ee9748aa3d1796f44d8a65a3a63e0c96360aa
Patrick N <pnetsmail-ms@yahoo.com>
2022-04-19 09:03:45 -0400

    Change to use Auth0 class (previously module.exports)

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 const router = express.Router();
-const auth0 = require ('../objects/auth0');
+import { Auth0 } from '../objects/auth0';
 const jwtAuthz = require('express-jwt-authz');
 import { logger } from '../objects/logger';
 
 router.post('/getUsers', jwtAuthz([ 'users:read' ]), async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const result: any = await auth0.getUsers(req);
+        const result: any = await Auth0.getUsers(req);
         res.json(result);
     } catch (err: any) {
         logger.log('error', 'Error in POST admin/getUsers', err);
@@ -16,7 +16,7 @@ router.post('/getUsers', jwtAuthz([ 'users:read' ]), async (req: express.Request
 
 router.post('/getUser',  jwtAuthz([ 'users:read' ]), async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const result: any = await auth0.getUser(req);
+        const result: any = await Auth0.getUser(req);
         res.json(result);
     } catch (err: any) {
         logger.log('error', 'Error in POST admin/getUsers', err);
@@ -26,7 +26,7 @@ router.post('/getUser',  jwtAuthz([ 'users:read' ]), async (req: express.Request
 
 router.post('/updateUser',  jwtAuthz([ 'users:read', 'users:write' ]), async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const result: any = await auth0.updateUser(req);
+        const result: any = await Auth0.updateUser(req);
         res.json(result);
     } catch (err: any) {
         logger.log('error', 'Error in POST admin/updateUser', err);
@@ -36,7 +36,7 @@ router.post('/updateUser',  jwtAuthz([ 'users:read', 'users:write' ]), async (re
 
 router.post('/getRoles',  jwtAuthz([ 'roles:read' ]), async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const result: any = await auth0.getRoles(req);
+        const result: any = await Auth0.getRoles(req);
         res.json(result);
     } catch (err: any) {
         logger.log('error', 'Error in POST admin/getRoles', err);
@@ -44,15 +44,17 @@ router.post('/getRoles',  jwtAuthz([ 'roles:read' ]), async (req: express.Reques
     }
 });
 
+/* For Stripe Integration
 router.post('/setUsersStripeCustomerId',  jwtAuthz([ 'roles:read' ]), async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
-        const result: any = await auth0.setUsersStripeCustomerId(req);
+        const result: any = await Auth0.setUsersStripeCustomerId(req);
         res.json(result);
     } catch (err: any) {
         logger.log('error', 'Error in POST admin/setUsersStripeCustomerId', err);
         res.status(err.statusCode).json(err);
     }
 });
+ */
 
 router.post('/getLogs',  jwtAuthz([ 'logs:read' ]), async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {

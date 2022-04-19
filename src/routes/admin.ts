49d6cba7919b24ded69ed8dae0c9aeb8f9aa15be
Patrick N <pnetsmail-ms@yahoo.com>
2022-04-19 05:46:22 -0400

    add types to caught errors, change logger from requires to object import

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -2,13 +2,13 @@ import express from 'express';
 const router = express.Router();
 const auth0 = require ('../objects/auth0');
 const jwtAuthz = require('express-jwt-authz');
-const logger = require('./../objects/logger');
+import { logger } from '../objects/logger';
 
 router.post('/getUsers', jwtAuthz([ 'users:read' ]), async (req: express.Request, res: express.Response, next: express.NextFunction) => {
     try {
         const result: any = await auth0.getUsers(req);
         res.json(result);
-    } catch (err) {
+    } catch (err: any) {
         logger.log('error', 'Error in POST admin/getUsers', err);
         res.status(err.statusCode).json(err);
     }
@@ -18,7 +18,7 @@ router.post('/getUser',  jwtAuthz([ 'users:read' ]), async (req: express.Request
     try {
         const result: any = await auth0.getUser(req);
         res.json(result);
-    } catch (err) {
+    } catch (err: any) {
         logger.log('error', 'Error in POST admin/getUsers', err);
         res.status(err.statusCode).json(err);
     }
@@ -28,7 +28,7 @@ router.post('/updateUser',  jwtAuthz([ 'users:read', 'users:write' ]), async (re
     try {
         const result: any = await auth0.updateUser(req);
         res.json(result);
-    } catch (err) {
+    } catch (err: any) {
         logger.log('error', 'Error in POST admin/updateUser', err);
         res.status(err.statusCode).json(err);
     }
@@ -38,7 +38,7 @@ router.post('/getRoles',  jwtAuthz([ 'roles:read' ]), async (req: express.Reques
     try {
         const result: any = await auth0.getRoles(req);
         res.json(result);
-    } catch (err) {
+    } catch (err: any) {
         logger.log('error', 'Error in POST admin/getRoles', err);
         res.status(err.statusCode).json(err);
     }
@@ -48,7 +48,7 @@ router.post('/setUsersStripeCustomerId',  jwtAuthz([ 'roles:read' ]), async (req
     try {
         const result: any = await auth0.setUsersStripeCustomerId(req);
         res.json(result);
-    } catch (err) {
+    } catch (err: any) {
         logger.log('error', 'Error in POST admin/setUsersStripeCustomerId', err);
         res.status(err.statusCode).json(err);
     }
@@ -58,7 +58,7 @@ router.post('/getLogs',  jwtAuthz([ 'logs:read' ]), async (req: express.Request,
     try {
         const result = await logger.getLogs(req);
         res.json(result);
-    } catch (err) {
+    } catch (err: any) {
         logger.log('error', 'Error in POST admin/getLogs', err);
         res.status(err.statusCode).json(err);
     }
